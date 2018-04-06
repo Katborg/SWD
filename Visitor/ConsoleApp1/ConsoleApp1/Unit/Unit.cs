6c15814884b6de368c3a98dc19d9157115be1b0e
Katborg <s.katborg@gmail.com>
2018-04-06 13:19:40 +0200

    projekt started

---
+++ Visitor/ConsoleApp1/ConsoleApp1/Unit/Unit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Unit
+{
+	abstract class Unit : IUnit
+	{
+		protected double Stregh;
+		protected double Agility;
+		protected double Inteligens;
+		public virtual double AcceptVisit()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	 public interface IUnit
+	 {
+	 }
+}
